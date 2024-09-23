96a53327c5684ffa0d739d6645bca767f039109e
Elcelcior <tototobey@gmail.com>
2024-09-23 18:41:00 -0400

    Added backround color, mood indicator, Emojis

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: DigitalPetApp(),
+    theme: ThemeData(),
   ));
 }
 
@@ -15,12 +16,18 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
   String petName = "Your Pet";
   int happinessLevel = 50;
   int hungerLevel = 50;
+  Color x = Colors.yellow;
+  Image myImage = Image.network('https://static.vecteezy.com/system/resources/previews/001/199/181/original/emoji-cat-face-neutral-png.png', 
+  width: 300,
+  height: 300);
+  String happinessState = "Neutral";
 
   // Function to increase happiness and update hunger when playing with the pet
   void _playWithPet() {
     setState(() {
       happinessLevel = (happinessLevel + 10).clamp(0, 100);
       _updateHunger();
+      CheckHappinessColor();  // Check color after updating happiness
     });
   }
 
@@ -29,6 +36,7 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
     setState(() {
       hungerLevel = (hungerLevel - 10).clamp(0, 100);
       _updateHappiness();
+      CheckHappinessColor();  // Check color after updating happiness
     });
   }
 
@@ -50,9 +58,35 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
     }
   }
 
+
+
+  // Check happiness and change background color
+  void CheckHappinessColor() {
+    if (happinessLevel > 70) {
+      x = Colors.green;
+      myImage = Image.network('https://static-00.iconduck.com/assets.00/smiling-cat-face-with-open-mouth-emoji-2048x2048-i1lhgbn1.png',
+      width: 300,
+      height: 300);
+      happinessState = "Happy";
+    } else if (happinessLevel >= 30) {
+      x = Colors.yellow;
+      myImage = Image.network('https://static.vecteezy.com/system/resources/previews/001/199/181/original/emoji-cat-face-neutral-png.png',
+      width: 300,
+      height: 300);
+      happinessState = "Neutral";
+    } else {
+      x = Colors.red;
+      myImage = Image.network('https://static-00.iconduck.com/assets.00/crying-cat-face-emoji-512x436-ba1f5eto.png',
+      width: 300,
+      height: 300);
+      happinessState = "Sad";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: x,
       appBar: AppBar(
         title: Text('Digital Pet'),
       ),
@@ -60,6 +94,7 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            myImage,
             Text(
               'Name: $petName',
               style: TextStyle(fontSize: 20.0),
@@ -72,6 +107,10 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
             SizedBox(height: 16.0),
             Text(
               'Hunger Level: $hungerLevel',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            Text(
+              'Happiness State: $happinessState',
               style: TextStyle(fontSize: 20.0),
             ),
             SizedBox(height: 32.0),
